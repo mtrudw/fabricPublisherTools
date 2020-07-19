3c336a4888a681e276a9edfacb9896b10180ee9a
Matthias Trudewind <matthias@die.trud.is>
2020-07-19 17:43:49 +0200

    added Snapper with basic position snapping

---
+++ src/Snapper.js
@@ -0,0 +1,85 @@
+import {fabric} from 'fabric';
+
+function fabricAddSnapper() {
+    if (fabric.Canvas.prototype._snapperAttached) {
+	return;
+    }
+
+    fabric.Canvas.prototype._snapperAttached = true;
+    fabric.Canvas.prototype.snapPoints =[];
+    fabric.Object.prototype.snapMargin =4;
+
+    fabric.Object.prototype.getBoundingCoords = function(calc) {
+	if (calc) {
+	    this.setCoords();
+	}
+	var center = this.getCenterPoint();
+	
+	var bounds = {'left': Math.min(this.aCoords.tl.x,this.aCoords.tr.x,this.aCoords.bl.x,this.aCoords.br.x),
+		      'top': Math.min(this.aCoords.tl.y,this.aCoords.tr.y,this.aCoords.bl.y,this.aCoords.br.y),
+		      'bottom': Math.max(this.aCoords.tl.y,this.aCoords.tr.y,this.aCoords.bl.y,this.aCoords.br.y),
+		      'right':  Math.max(this.aCoords.tl.x,this.aCoords.tr.x,this.aCoords.bl.x,this.aCoords.br.x),
+		      'centerX': center.x,
+		      'centerY': center.y};
+	return bounds;
+    }
+
+    fabric.Object.prototype.setByBoundingCoords = function(coords) {
+	this.setPositionByOrigin(new fabric.Point(coords.centerX, coords.centerY),'center','center');
+    }
+    
+    fabric.Object.prototype.getSnapPoints = function() {
+	return {'h':[400,800] , 'v':[400,800]};
+    }
+
+    
+    
+    fabric.Object.prototype._snapMove = function() {
+	var coords = this.getBoundingCoords(true),
+	    snaps = this.getSnapPoints(),
+	    height = coords.bottom - coords.top,
+	    width = coords.right-coords.left;
+	var setCoords = coords;
+
+	function inRange (x1,x2,range) {
+	    return Math.abs(x1 - x2) <= range;
+	}
+
+
+	//snap Y
+	if (inRange(coords.top,snaps.v[0],this.snapMargin)) {
+	    setCoords.top = snaps.v[0];
+	    setCoords.bottom = snaps.v[0] +height;
+	    setCoords.centerY = snaps.v[0] + height/2;
+	}
+	else if (inRange(coords.centerY,snaps.v[0],this.snapMargin)) {
+	    setCoords.top = snaps.v[0] - height/2;
+	    setCoords.bottom = snaps.v[0] + height/2;
+	    setCoords.centerY = snaps.v[0];
+	} else if (inRange(coords.bottom,snaps.v[0],this.snapMargin)) {
+	    setCoords.top = snaps.v[0] - height;
+	    setCoords.bottom = snaps.v[0];
+	    setCoords.centerY = snaps.v[0]-height/2;
+	}
+
+	//snap X
+	if (inRange(coords.left,snaps.h[0],this.snapMargin)) {
+	    setCoords.left = snaps.h[0];
+	    setCoords.right = snaps.h[0] +width;
+	    setCoords.centerX = snaps.h[0] + width/2;
+	}
+	else if (inRange(coords.centerX,snaps.h[0],this.snapMargin)) {
+	    setCoords.left = snaps.h[0] - width/2;
+	    setCoords.right = snaps.h[0] + width/2;
+	    setCoords.centerX = snaps.h[0];
+	} else if (inRange(coords.right,snaps.h[0],this.snapMargin)) {
+	    setCoords.left = snaps.h[0] - width;
+	    setCoords.right = snaps.h[0];
+	    setCoords.centerX = snaps.h[0]-width/2;
+	}
+	    
+	this.setByBoundingCoords(setCoords);
+    }
+}
+
+export {fabricAddSnapper};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import {ImageContainer} from './ImageContainer.js';
 import {fabricAddObjectIDs} from './ObjectIds.js';
 import {fabricAddUndoRedo} from './UndoRedo.js';
+import {fabricAddSnapper} from './Snapper.js';
 
 export {
     ImageContainer,
     fabricAddObjectIDs,
-    fabricAddUndoRedo    
+    fabricAddUndoRedo,
+    fabricAddSnapper
 }
 
